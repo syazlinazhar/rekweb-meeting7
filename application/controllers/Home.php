fe0370139a247a0e09889c36808fe4e9811d657b
unknown <amierasyazlin97@gmail.com>
2019-10-22 02:54:36 +0700

    create folder templates

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,7 +3,9 @@
 class Home extends CI_Controller {
     public function index()
     {
-       $this->load->view('home/index');
+        $this->load->view('templates/header');
+        $this->load->view('home/index');
+        $this->load->view('templates/footer');
     }
 
 }
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home / Index</title>
-</head>
-<body>
-    <h1>Home / Index</h1>
-    
-</body>
-</html>
+
+    <h1>Hello, world!</h1>
+
+   
