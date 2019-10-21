8ff58319ea4fd0f8d27a051b5b2bc7efa33c4727
unknown <amierasyazlin97@gmail.com>
2019-10-22 03:08:46 +0700

    send data from URL

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,9 +1,10 @@
 <?php
 
 class Home extends CI_Controller {
-    public function index()
+    public function index($name)
     {
         $data['title'] = 'Home Page';
+        $data['name'] = $name;
         $this->load->view('templates/header', $data);
         $this->load->view('home/index');
         $this->load->view('templates/footer');
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,6 +13,7 @@
   <body>
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
   <a class="navbar-brand" href="#">CI App</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -22,7 +23,7 @@
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" href="#">Mahasiswa</a>
       <a class="nav-item nav-link" href="#">About</a>
-      
+      </div>
     </div>
   </div>
 </nav>
