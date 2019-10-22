248e0710d15d4d07d4c1df238feca521b37ad64d
unknown <amierasyazlin97@gmail.com>
2019-10-22 21:30:20 +0700

    create button add data

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -2,6 +2,12 @@
 
 <div class="row mt-3">
     <div class="col-md-6">
+        <a href="" class="btn btn-primary">Add Data Mahasiswa</a>
+    </div>
+</div>
+
+<div class="row mt-3">
+    <div class="col-md-6">
     <h3>Register Mahasiswa</h3>
         <ul class="list-group">
         <?php foreach( $mahasiswa as $mhs ) : ?>
