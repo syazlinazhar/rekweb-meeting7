c69fb93daebb0c9edafab839e4c0c9851ada483b
unknown <amierasyazlin97@gmail.com>
2019-10-22 22:42:12 +0700

    add data mahasiswa

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -5,4 +5,16 @@ class Mahasiswa_model extends CI_model {
         {
             return $this->db->get('mahasiswa')->result_array();
         }
+
+        public function addDataMahasiswa()
+        {
+            $data = [
+                "name" => $this->input->post('name', true),
+                "matric" => $this->input->post('matric', true),
+                "email" => $this->input->post('email', true),
+                "course" => $this->input->post('course', true),
+            ];
+
+            $this->db->insert('mahasiswa', $data);
+        }
 }
