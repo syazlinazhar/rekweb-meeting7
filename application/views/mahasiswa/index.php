0f201f94fef09d85515f97a4da8a4f41e3aa7983
unknown <amierasyazlin97@gmail.com>
2019-10-22 22:57:56 +0700

    flash data sentence

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -1,4 +1,16 @@
 <div class="container">
+    <?php if( $this->session->flashdata('flash') ) : ?>
+    <div class="row mt-3">
+        <div class="col-md-6">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                Data was <strong>successfully</strong> <?= $this->session->flashdata('flash'); ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
 
 <div class="row mt-3">
     <div class="col-md-6">
