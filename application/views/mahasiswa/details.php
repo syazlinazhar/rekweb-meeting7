d946b1ed4295d4d55bda4c2f0fa73dd5d33e57de
unknown <amierasyazlin97@gmail.com>
2019-10-23 00:22:18 +0700

    crete new file details

---
+++ application/views/mahasiswa/details.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+                <div class="card">
+        <div class="card-header">
+                Details Mahasiswa
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title"><?= $mahasiswa['name']; ?></h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
