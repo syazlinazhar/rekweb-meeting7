6af1cea5ba7ea1c620ddebc2e41c19a19ab189be
unknown <amierasyazlin97@gmail.com>
2019-10-23 00:23:39 +0700

    function getMahasiswaById

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -21,6 +21,11 @@ class Mahasiswa_model extends CI_model {
         public function deleteDataMahasiswa($id)
         {
             $this->db->where('id', $id);
-            $this->db->delete('mahasiswa');
+            $this->db->delete('mahasiswa', ['id' => $id]);
+        }
+
+        public function getMahasiswaById($id)
+        {
+            return $this->db->get_where('mahasiswa', ['id' => $id])->row_array();
         }
 }
