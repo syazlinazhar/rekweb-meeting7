b237f3595a3ea166b9c390be2dea9b40c425766c
unknown <amierasyazlin97@gmail.com>
2019-10-23 02:33:08 +0700

    create button find

--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -17,9 +17,31 @@
         <a href="<?= base_url(); ?>mahasiswa/add" class="btn btn-primary">Add Data Mahasiswa</a>
     </div>
 </div>
+
+<div class="row mt-3">
+    <div class="col-md-6">
+        <form action="" method="post">
+            <div class="input-group">
+                <input type="text" class="form-control" placeholder="Find Data Mahasiswa.." name="keyword">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="submit">Find</button>
+            </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+
 <div class="row mt-3">
     <div class="col-md-6">
     <h3>Register Mahasiswa</h3>
+
+    <?php if( empty($mahasiswa) ) : ?>
+            <div class="alert alert-danger" role="alert">
+        Data is not found. 
+        </div>
+    <?php endif; ?>
+
         <ul class="list-group">
         <?php foreach( $mahasiswa as $mhs ) : ?>
             <li class="list-group-item"><?= $mhs['name'] ?>
