0f95c9ec94ab9569e0f096f2f152d1b5e32d3429
unknown <amierasyazlin97@gmail.com>
2019-10-22 22:43:50 +0700

    run form validation

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -5,6 +5,7 @@ class Mahasiswa extends CI_Controller {
     {
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->load->library('form_validation');
     }
 
     
@@ -21,9 +22,18 @@ class Mahasiswa extends CI_Controller {
     {
         $data['title'] = 'Form Add Data';
 
-        $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/add');
-        $this->load->view('templates/footer');
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('matric', 'Matric', 'required|numeric');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+        if( $this->form_validation->run() == FALSE ) {
+            $this->load->view('templates/header', $data);
+            $this->load->view('mahasiswa/add');
+            $this->load->view('templates/footer');
+        } else {
+            $this->Mahasiswa_model->addDataMahasiswa();
+            redirect('mahasiswa');
+        }
 
     }
 }
