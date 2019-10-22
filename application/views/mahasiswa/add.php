d52df9f920e8952b3bc844258e43fa01b8b68a51
unknown <amierasyazlin97@gmail.com>
2019-10-22 21:37:47 +0700

    create new file add

---
+++ application/views/mahasiswa/add.php
@@ -0,0 +1,23 @@
+<div class="container">
+
+    <div class="row mt-3">
+        <div class="col-md-6">
+
+        <div class="card">
+            <div class="card-header">
+                Form Add Data Mahasiswa
+            </div>
+            <div class="card-body">
+
+            <form action="" method="post">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name">
+            </div>
+        </form>
+            </div>
+        </div>
+
+    </div>
+    </div>
+</div>
