c02ff74a766011d1856cd701ecaa25f9a72a956b
unknown <amierasyazlin97@gmail.com>
2019-10-23 02:32:39 +0700

    create find data using keyword

--- application/models/Mahasiswa_model.php
+++ application/models/Mahasiswa_model.php
@@ -41,4 +41,14 @@ class Mahasiswa_model extends CI_model {
             $this->db->where('id', $this->input->post('id'));
             $this->db->update('mahasiswa', $data);
         }
+
+        public function findDataMahasiswa()
+        {
+            $keyword = $this->input->post('keyword', true);
+            $this->db->like('name', $keyword);
+            $this->db->or_like('matric', $keyword);
+            $this->db->or_like('email', $keyword);
+            $this->db->or_like('course', $keyword);
+            return $this->db->get('mahasiswa')->result_array();
+        }
 }
