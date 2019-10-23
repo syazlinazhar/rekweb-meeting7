4163c029db994311406dac1cf31de9e359c8b7d7
unknown <amierasyazlin97@gmail.com>
2019-10-23 20:21:27 +0700

    create Peoples_model

---
+++ application/controllers/Peoples.php
@@ -0,0 +1,90 @@
+<?php
+
+class Peoples extends CI_Controller { 
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    //     $this->load->model('Mahasiswa_model');
+    //     $this->load->library('form_validation');
+    // }
+
+    public function index()
+    {
+
+        $data['title'] = 'List of Peoples';
+        
+        $this->load->model('Peoples_model', 'peoples');
+        
+        $data['peoples'] = $this->peoples->getAllPeoples();
+        
+        // if( $this->input->post('keyword') ) {
+        //     $data['mahasiswa'] = $this->Mahasiswa_model->findDataMahasiswa();
+        // }
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('peoples/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    // public function add()
+    // {
+    //     $data['title'] = 'Form Add Data';
+
+    //     $this->form_validation->set_rules('name', 'Name', 'required');
+    //     $this->form_validation->set_rules('matric', 'Matric', 'required|numeric');
+    //     $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+    //     if( $this->form_validation->run() == FALSE ) {
+    //         $this->load->view('templates/header', $data);
+    //         $this->load->view('mahasiswa/add');
+    //         $this->load->view('templates/footer');
+    //     } else {
+    //         $this->Mahasiswa_model->addDataMahasiswa();
+    //         $this->session->set_flashdata('flash', 'Added');
+    //         redirect('mahasiswa');
+    //     }
+
+    // }
+
+    // public function delete($id)
+    // {
+    //     $this->Mahasiswa_model->deleteDataMahasiswa($id);
+    //     $this->session->set_flashdata('flash', 'Deleted');
+    //     redirect('mahasiswa');
+    // }
+
+    // public function details($id)
+    // {
+    //     $data['title'] = 'Details Mahasiswa';
+    //     $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+    //     $this->load->view('templates/header', $data);
+    //     $this->load->view('mahasiswa/details', $data);
+    //     $this->load->view('templates/footer');
+    // }
+
+    // public function edit($id)
+    // {
+    //     $data['title'] = 'Form Edit Data';
+    //     $data['mahasiswa'] = $this->Mahasiswa_model->getMahasiswaById($id);
+    //     $data['course'] = ['Information Technology', 'Science Economy', 'Human Resource', 'Accountancy', 'Communication'];
+
+    //     $this->form_validation->set_rules('name', 'Name', 'required');
+    //     $this->form_validation->set_rules('matric', 'Matric', 'required|numeric');
+    //     $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+
+    //     if( $this->form_validation->run() == FALSE ) {
+    //         $this->load->view('templates/header', $data);
+    //         $this->load->view('mahasiswa/edit', $data);
+    //         $this->load->view('templates/footer');
+    //     } else {
+    //         $this->Mahasiswa_model->editDataMahasiswa();
+    //         $this->session->set_flashdata('flash', 'Edited');
+    //         redirect('mahasiswa');
+    //     }
+
+    // }
+
+}
+
+
+
